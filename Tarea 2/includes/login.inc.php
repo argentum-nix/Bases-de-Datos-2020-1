028b9ceb4f064c777954173ebad69a9cb466cebc
Anastasiia Fedorova <miffyrli@gmail.com>
2020-07-17 18:36:00 -0400

    navbar done + error corrections

--- Tarea 2/includes/login.inc.php
+++ Tarea 2/includes/login.inc.php
@@ -26,51 +26,30 @@ if(isset($_POST['login-submit'])) {
 		$res = mysqli_stmt_get_result($statement);
 		$fila = mysqli_fetch_assoc($res);
 		if($fila){
+			$nom = $fila["nombre"];
+			$uid = $fila["id_persona"];
 			$checkpwd = password_verify($pwd_usuario, $fila['password']);
 			if($checkpwd == true){
 				session_start();
-				$_SESSION["id"] = $fila["id_persona"];
-				$_SESSION["nombre"] = $fila["nombre"];
-
-				// Revisa si es usuario o si es artista
-				// Dependiendo de tipo de usuario voy a esconder botones de CRUD.
-				$flag = 0;
-				$sql_query = "SELECT * FROM usuarios WHERE id_usuario=?";
-				$statement = mysqli_stmt_init($connection);
-				if (!mysqli_stmt_prepare($statement, $sql_query)) {
-					header("Location: ../login.php?error=sqlerror");
-					exit();
-				}
-				mysqli_stmt_bind_param($statement, "i", $fila["id_persona"]);
-				mysqli_stmt_execute($statement);
-				mysqli_stmt_store_result($statement);
-				$check = mysqli_stmt_num_rows($statement);
-				if($check = 1){
-					$_SESSION["usertype"] = "user";
-					$flag = 1;
-				}
-				$sql_query = "SELECT * FROM artistas WHERE id_artista=?";
-				$statement = mysqli_stmt_init($connection);
-				if (!mysqli_stmt_prepare($statement, $sql_query)) {
-					header("Location: ../login.php?error=sqlerror");
-					exit();
-				}
-				mysqli_stmt_bind_param($statement, "i", $fila["id_persona"]);
-				mysqli_stmt_execute($statement);
-				mysqli_stmt_store_result($statement);
-				$check = mysqli_stmt_num_rows($statement);
-				if($check = 1){
-					$_SESSION["usertype"] = "artist";
-					$flag = 1;
-				}
-
-				if($flag == 0){
-					header("Location: ../login.php?error=sqlerror");
+				$_SESSION["id"] = $uid;
+				$_SESSION["nombre"] = $nom;
+				$res= mysqli_query($connection, "SELECT * FROM usuarios WHERE id_usuario='$uid'");
+				if(mysqli_num_rows($res) > 0){
+					$_SESSION['usertype'] = 'user';
+					header("Location: ../index.php?login=success");
 					exit();
 				}
 				else{
-					header("Location: ../index.php?login=success");
-					exit();
+					$res = mysqli_query($connection, "SELECT * FROM artistas WHERE id_artista='$uid'");
+					if(mysqli_num_rows($res) > 0){
+						$_SESSION['usertype'] = 'artist';
+						header("Location: ../index.php?login=success");
+						exit();
+					}
+					else{
+						header("Location: ../login.php?error=sqlerror");
+						exit();
+					}
 				}
 			}
 
@@ -86,7 +65,8 @@ if(isset($_POST['login-submit'])) {
 	}
 	mysqli_stmt_close($statement);
 	mysqli_close($connection);
-}
+	}
+
 else{
 	header("Location: ../login.php");
 	exit();
--- Tarea 2/index.php
+++ Tarea 2/index.php
@@ -14,105 +14,97 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-
-
 </head>
+
 <body>
-		<div class="main-container">
-			<div class="topbar">
-				  <li class="dropdown">
-				    <a href="javascript:void(0)" class="nav-link"> <?php echo $_SESSION['nombre'];?> </a>
-				    <div class="dropdown-content">
-				    <div class="nav-item">
-						 		<a href="#" class="nav-link">
-						 			
-						 			<span>Perfil</span>
-	                				
-	                			</a>
-	                </div>
-				       <div class="nav-item">
-						 		<a href="#" class="nav-link">
-						 			
-						 			<span>Cuenta</span>
-	                				
-	                			</a>
-	                </div>
-				       <div class="nav-item">
-						 		<a href="#" class="nav-link">
-						 			<i class="fas fa-sign-out-alt"></i>
-						 			<span>Salir</span>
-	                				
-	                			</a>
-	                </div>
-				    </div>
-				  </li>
-			</div>
-			<div class="wrapper">
-				<nav class="sidebar">
-					<div class="sidebar-header">
-						<img src="img/poyofy.png" alt="poyofy" width=150px>
-					</div>
+	<div class="main-container">
+		<div class="topbar">
+			<div class="dropdown menu ml-3"style="width: 100px; position: absolute; right:3%; margin-right: 28px; top: 10px;">
+				<button type="button" class="d-flex align-items-center" data-toggle="dropdown">
 
-					<div class="sidebar-nav">
-						<div class="nav-item">
-						 		<a href="#" class="nav-link">
-						 			<i class="fas fa-home"></i>
-						 			<span>Inicio</span>
-	                				
-	                			</a>
-	           			</div>
-	           			<div class="nav-item">
-						 	<div> 
+					<?php if($_SESSION['usertype'] == 'user'):?>
+						<img src="img/user_profile.png" class="profile" width=150px>
+					<?php else: ?>
+						<img src="img/artist_profile.png" class="profile" width=150px>
+					<?php endif; ?>
+
+					<span><?php echo $_SESSION['nombre'];?></span>
+					<i class="fas fa-caret-down ml-2 mr-2"></i>
+				</button>
+
+				<div class="dropdown-menu mt-0 p-0">
+					<a href="#" class="dropdown-item">Cuenta</a>
+					<a href="#" class="dropdown-item">Perfil</a>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item">Salir</a>
+				</div>
+			</div>
+		</div>
+		<div class="wrapper">
+			<nav class="sidebar">
+				<div class="sidebar-header">
+					<img src="img/poyofy.png" alt="poyofy" width=150px>
+				</div>
+
+				<div class="sidebar-nav">
+					<div class="nav-item">
+						<a href="#" class="nav-link">
+							 <i class="fas fa-home"></i>
+							 <span>Inicio</span>
+						</a>
+	           		</div>
+	           		<div class="nav-item">
+	                	<a href="#" class="nav-link">
+	                		<i class="fas fa-search"></i>
+	                		<span>Buscar</span>
+	                	</a>
+	           		</div>
+	            	<div class="nav-item">
+	                	<a href="#" class="nav-link">
+	                		<i class="far fa-bookmark"></i>
+	                		<span>Tu biblioteca</span>
+	                	</a>
+	           		</div>
+	           		<!-- Muestra contenido segun el tipo de usuario OJO mal echo de tipo de usuario :C-->
+	           		<?php if($_SESSION['usertype'] == 'user'):?>
+	           			<div class="nav-create d-flex flex-column mt-4">
+	           				<h2>Playlists</h2>
+	           				<div class="nav-item">
 	                			<a href="#" class="nav-link">
-	                				<i class="fas fa-search"></i>
-	                				<span>Buscar</span>
+	                				<i class="fas fa-plus-circle"></i>
+	                				<span>Nuevo playlist</span>
+	                			</a>
+	           			 	</div>
+	           			 	<div class="nav-item">
+	                			<a href="#" class="nav-link">
+	                				<i class="fas fa-heart"></i>
+	                				<span>Canciones favoritas</span>
 	                			</a>
 	           			 	</div>
 	           			</div>
 
-	            		<div class="nav-item">
-						 	<div> 
+	           		<?php else: ?>
+	           			<div class="nav-create d-flex flex-column mt-4">
+	           				<h2>Studio</h2>
+	           				<div class="nav-item">
+						 		<div> 
+	                				<a href="#" class="nav-link">
+	                					<i class="fas fa-music"></i>
+	                					<span>Nueva canción</span>
+	                				</a>
+	           			 		</div>
+	           				</div>
+	           				<div class="nav-item">
 	                			<a href="#" class="nav-link">
-	                				<i class="far fa-bookmark"></i>
-	                				<span>Tu biblioteca</span>
+	                				<i class="fas fa-plus-circle"></i>
+	                				<span>Nuevo album</span>
 	                			</a>
-	           			 	</div>
+	           				</div>
 	           			</div>
-
-	           			<!-- Muestra contenido segun el tipo de usuario OJO mal echo de tipo de usuario :C-->
-	           			<?php if($_SESSION['usertype'] == 'user'):?>
-	           				<div class="nav-create d-flex flex-column mt-4">
-	           					<h2>Playlists</h2>
-	           				</div>
-	           			<?php else: ?>
-	           				<div class="nav-create d-flex flex-column mt-4">
-	           					<h2>Studio</h2>
-	           					<div class="nav-item">
-						 			<div> 
-	                					<a href="#" class="nav-link">
-	                						<i class="fas fa-music"></i>
-	                						<span>Nueva canción</span>
-	                					</a>
-	           			 			</div>
-	           					</div>
-	           					<div class="nav-item">
-						 			<div> 
-	                					<a href="#" class="nav-link">
-	                						<i class="fas fa-plus-circle"></i>
-	                						<span>Nuevo album</span>
-	                					</a>
-	           			 			</div>
-	           					</div>
-	           				</div>
-	           			<?php endif; ?>
-
-					</div>
-
-				</nav>
-			</div>
-			</div> 
-
-    </div>
-
+	           		<?php endif; ?>
+	           	</div>
+			</nav>
+		</div>
+	</div> 
 </body>
 </html>
